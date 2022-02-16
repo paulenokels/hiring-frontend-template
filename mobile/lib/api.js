d44e8d7aa1f93252f98acff665b3c3ab10bc04e8
Acheme Paul <achemepaulenokela@gmail.com>
2022-02-16 13:25:18 +0100

    Added fetch devices to lib/api

--- mobile/lib/api.js
+++ mobile/lib/api.js
@@ -1,3 +1,11 @@
 export function fetchUsers() {
   return fetch("http://localhost:4000/api/users").then((res) => res.json());
 }
+
+export function fetchDevices(filterOptions) {
+  let url = "http://localhost:4000/api/devices";
+  if (filterOptions) {
+    url += filterOptions;
+  }
+  return fetch(url).then((res) => res.json());
+}
